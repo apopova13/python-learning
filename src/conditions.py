1cabdb409f8444643c30e7dabf8e460b028c92f5
Alena <a.popova1320@mail.ru>
2022-12-17 16:12:32 +0500

    Условные конструкции Python

---
+++ src/conditions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+a = 5
+b = 2
+
+if a > b:
+    print('a больше b')
+
+if a == b:
+    print('a равно b')
+else:
+    print('a не равно b')
+
+if a != b and b > 0:
+    print('a не равно b и b больше 0')
+
+if a != b or b > 0:
+    print('a не равно b или b больше 0')
