74d41bc164404b451f0bfd020a5ef277d578813a
Alena <a.popova1320@mail.ru>
2022-12-17 17:43:39 +0500

    Работа со строками в Python

---
+++ src/strings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+a = 'Привет'
+b = 'Мир'
+c = a + ' ' + b
+
+print('Исходная строка: ' + c)
+
+print(c.lower())
+print(c.upper())
+print(c.capitalize())
+print(c.title())
+print(c.swapcase())
+
+c = a + '\n' + b
+print(c)
+print('\\')
+print(r'C:\Windows\System32\oobe')
